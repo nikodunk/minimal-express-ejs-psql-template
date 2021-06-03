e717d741b68d06ffa9731c05cc204f7336e6ca51
Niko <n.dunkel@gmail.com>
2021-06-03 00:09:33 -0400

    really the last for tonight

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { createUsers } = require("../../model");
 
 router.post("/csv", async (req, res, next) => {
-  console.log(req.body.token);
   await createUsers(req.body.token);
   res.send("worked").status(200);
 });
