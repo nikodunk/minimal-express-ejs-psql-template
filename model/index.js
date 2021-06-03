de09fa6446bef47c0bd4277a2b69733176379172
Niko <n.dunkel@gmail.com>
2021-06-02 23:51:33 -0400

    working

--- model/index.js
+++ model/index.js
@@ -1,5 +1,5 @@
 const { Client } = require("pg");
-const client = new Client(process.env.DATABASE_URL);
+const client = new Client(process.env.DATABASE_URL + "?ssl=true");
 client.connect();
 
 exports.getUsers = async () => {
