d9cbc9a43ad95edb4933eadb6a32ac06d26206e3
Niko <n.dunkel@gmail.com>
2021-06-02 23:21:56 -0400

    test

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const { createUsers } = require("../../model");
 
 router.post("/csv", async (req, res, next) => {
-  createUsers(req.body.token);
+  console.log(req.body.token);
+  await createUsers(req.body.token);
   res.send("worked").status(200);
 });
 
